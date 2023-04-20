9c398b4bc80e07dac566fcab7f643a651b0fef40
Julia Wenger <julia.wenger@kellogg.northwestern.edu>
2023-04-20 00:28:45 +0000

    Notes from Class #1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,41 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3 
+puts 2
 
 # Perform simple math with numbers
+## knows the order of operations
+puts 3 + 2 
+puts 3*2 
+puts 3 / 2
+
+puts 5 * (2-2)
 
 # Strings
 
+puts "Hello World"
+
 # Combine strings together
+puts "Hello " + "World"
+
+puts "Tacos are delish"
+
+puts "Tacos" * 3
 
 # Variables
+food = "Tacos"
+number = 3 
+
+puts food * number 
 
 # Combine strings and variables
+greeting = "Heyyyyyy"
+
+puts "#{greeting}, world!"
+
+welcome_message = "#{greeting}, world!"
 
 # String manipulation
+puts welcome_message.upcase   ##UPPERCASE letters vs. reading backward##
+puts welcome_message.reverse
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,48 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# puts true
+# puts fals 
 
 # Boolean Expressions
 
+#puts 3 < 2 
+#puts 3 > 2
+#puts 3 != 2
+
 # If Conditional Logic
+#if 3 > 2
+    # do something 
+  #  puts "math works"
+#end 
+
+#if 5 > 4 
+    #puts "oh dear"
+#end 
 
 # If/Else Conditional Logic
+ password = "Tacos"
+ user_entered_password= "puppies"
+
+ if user_entered_password == password
+    puts "welcome, you're in!"
+ else 
+    puts "try again"
+ end
+
 
 # Elsif Conditional Logic
+home_team = 1
+away_team = 2
+if home_team > away_team 
+    puts "winner!!"
+elsif home_team == away_team
+    puts "tied..meh"
+else 
+    puts "loser :("
+end 
 
 # Combining Expressions
+# && combines expressions
+# if  temp > 60 && condition == "sunny"
+# || means OR (when combining expressions as an and/or)
