62d85005c5eb37670f394c0e01d99147a83cb50c
Julia Wenger <julia.wenger@kellogg.northwestern.edu>
2023-04-20 02:14:11 +0000

    Update after Loops

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,26 @@
 #   puts "tacos!"
 # end
 
+
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado", "mushroom"]
+
+
+index = 0 
+loop do
+    if index == 5
+        break 
+    end 
+    taco = tacos[index]
+# means you are reaching into the array at subsequent sections within the taco array
+
+    puts "#{taco} tacos"
+index = index + 1
+end 
+
+# means that you have gone through 5 times, and are actually on your 6th time through
+
+for taco in tacos
+    # do something 
+    puts "#{taco} tacos"
+end 
